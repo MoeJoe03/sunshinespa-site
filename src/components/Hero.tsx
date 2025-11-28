01c7f01730c06b6bd6e12ad922599354e19c37fc
Mohammed Yusuf Jogee <myusufjogee@gmail.com>
2025-11-28 15:17:55 +0200

    remove mouse

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,13 +46,6 @@ const Hero = () => {
           </Button>
         </div>
       </div>
-
-      {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-float">
-        <div className="w-6 h-10 border-2 border-primary/50 rounded-full p-1">
-          <div className="w-1.5 h-3 bg-primary rounded-full mx-auto animate-pulse" />
-        </div>
-      </div>
     </section>
   );
 };
